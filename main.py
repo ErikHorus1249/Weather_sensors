6038eee8a6a9297577b0c5c60fa5df92ddbe11e0
ErikHorus1249 <tuananh1421999@gmail.com>
2022-01-24 23:55:26 +0700

    the first commit

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 from fastapi import FastAPI
-from fastapi import APIRouter
-from database.Models import *
-from database.Connect import conn
-from bson.objectid import ObjectId
-from features import *
-from features.DateTime import  getUpdateTime, now
+from routes.Sensor import sensor
 
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-    return {"data":True}
+# @app.get("/")
+# def get_root():
+#     return {"data":True}
 
 
 # route API 
-# app.include_router(sensor)
-
-@app.post("/Sensors", response_description="Add new sensor data", response_model=SensorSavingModel)
-async def create_data(senData: SensorEntryModel):
-    
-    doc = dict((k, v) for k, v in senData.dict().items() if v is not None)
-    doc['updatedAt'] = getUpdateTime()
-    doc['timestamp'] = now()
-    
-    # fdoc = SensorSavingModel.parse_obj()
-        
-    if res := conn.Sensor.insert_one(doc):
-        return SensorSavingModel.parse_obj(conn.Sensor.find_one({'_id':ObjectId(res.inserted_id)})) 
-    else:
-        return False
+app.include_router(sensor)
