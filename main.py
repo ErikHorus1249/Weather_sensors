f5d34f188f1ec4b2f4d6e5e2d3f83dbec447410d
ErikHorus1249 <tuananh1421999@gmail.com>
2022-01-24 23:39:48 +0700

    the first commit

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def get_root():
 # route API 
 # app.include_router(sensor)
 
-@app.post("/Sensors", response_description="Add new sensor data", response_model=SensorSavingModel)
-async def create_data(senData: SensorEntryModel):
+# @app.post("/Sensors", response_description="Add new sensor data", response_model=SensorSavingModel)
+# async def create_data(senData: SensorEntryModel):
     
-    doc = dict((k, v) for k, v in senData.dict().items() if v is not None)
-    doc['updatedAt'] = getUpdateTime()
-    doc['timestamp'] = now()
+#     doc = dict((k, v) for k, v in senData.dict().items() if v is not None)
+#     doc['updatedAt'] = getUpdateTime()
+#     doc['timestamp'] = now()
     
-    # fdoc = SensorSavingModel.parse_obj()
+#     # fdoc = SensorSavingModel.parse_obj()
         
-    if res := conn.Sensor.insert_one(doc):
-        return SensorSavingModel.parse_obj(conn.Sensor.find_one({'_id':ObjectId(res.inserted_id)})) 
-    else:
-        return False
+#     if res := conn.Sensor.insert_one(doc):
+#         return SensorSavingModel.parse_obj(conn.Sensor.find_one({'_id':ObjectId(res.inserted_id)})) 
+#     else:
+#         return False
