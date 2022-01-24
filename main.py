3fb82d1d0b5028b25a3ae8d78c62709e88c0c033
ErikHorus1249 <tuananh1421999@gmail.com>
2022-01-24 23:32:39 +0700

    the first commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from routes.Sensor import sensor
 
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-    return {"data":True}
+# @app.get("/")
+# def get_root():
+#     return {"data":True}
+
+
+# route API 
+app.include_router(sensor)
